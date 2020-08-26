ea29a0e794474c3f6bb7f560fb52e4e75fc72c3a
raj patel <kakhilesh79@gmail.com>
2020-08-26 09:52:01 +0530

    the last night work

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -4,9 +4,11 @@ import {connect} from 'react-redux'
 import { fetchPosts } from '../actions';
 
  class PostList extends React.Component{
+     
      componentDidMount(){
          this.props.fetchPosts();
      }
+
     render(){
         return(
             <div>Post List</div>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import {combineReducers} from 'redux'
+import postReducer from './postReducer';
 
 
 export default combineReducers({
-    dummy:()=>10
+    posts:postReducer
 });
